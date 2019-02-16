d3047fd5093faa93e47e7e7c3ca48beef38cbc36
kuhblume <kasumigaura0524@yahoo.co.jp>
2019-02-17 02:47:45 +0900

    'それっぽいスライドがまあまあ実装できて来た'

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
 let tabCase = document.getElementsByClassName('tab-case');
-let tabs=[];
-for(let i = 0; i < tabCase.length; i++){
-    // /* getElementsTagNameを使う*/
-    // findDiv1 = hoge[i].getElementsByTagName('div');
-    // /* childrenを使って要素の番号を指定する*/
-    // findDiv2 = hoge[i].children[1];
+let tabs=tabCase[0].children;
 
-    // tabCase[i].getElementsByClassName('div').disabled=true;
+let tabLocate=['0','-400%','-300%','-200%','-100%'];
+//
+// document.getElementById("left").onclick = function() {
+//     console.log(tabCase[0].children[3].style.left);
+//     tabCase[0].children[0].style.left='100%';
+//     tabCase[0].children[1].style.left='0';
+//     tabLocate.push(tabLocate.shift());
+//
+//     console.log(tabLocate);
+// };
+
+function left() {
+    tabLocate.unshift(tabLocate.pop());
+    for (let i = 0; i < tabLocate.length; i++) {
+        tabs[i].style.left=tabLocate[i];
+    }
+    window.scrollTo(0,0);
 }
-
-
-document.getElementById("a").onclick = function() {
-    console.log(tabCase[0].children);
-    tabCase[0].children[0].style.left='100%';
-    tabCase[0].children[1].style.left='0';
-    // tabCase[0].getElementsByClassName('tag')[0].disabled=true;
-    // document.getElementsByClassName('tag')[0].style.color="white";
-};
+function right() {
+    tabLocate.push(tabLocate.shift());
+    for (let i = 0; i < tabLocate.length; i++) {
+        tabs[i].style.left=tabLocate[i];
+    }
+    window.scrollTo(0,0);
+}
