9bc833af30762ada4dec25b73a376e09cd6d03d9
kuhblume <kasumigaura0524@yahoo.co.jp>
2019-02-21 00:53:26 +0900

    スライドの切り替えとアニメーション

--- js/main.js
+++ js/main.js
@@ -1,28 +1,52 @@
 let tabCase = document.getElementsByClassName('tab-case');
 let tabs=tabCase[0].children;
+let radios = document.getElementsByName('radio');
+// let tabLocate=['0','-400%','-300%','-200%','-100%'];
+// let tabLocate=['0','-100%','-100%','100%','100%'];
 
-let tabLocate=['0','-400%','-300%','-200%','-100%'];
-//
-// document.getElementById("left").onclick = function() {
-//     console.log(tabCase[0].children[3].style.left);
-//     tabCase[0].children[0].style.left='100%';
-//     tabCase[0].children[1].style.left='0';
-//     tabLocate.push(tabLocate.shift());
-//
-//     console.log(tabLocate);
-// };
+//本当なら左右にスライドさせたかったが高さがtab内の高さがばらばらだとスライドバーが高さの差だけおかしくなる
+//見えない要素を左側におくと高さが可変状態？になる
+//右下はhiddenでもシークバーで無理やり伸ばされるけど左上の見えない要素はシークバーで延ばされないっぽい
+let tabLocate=['0','-100%','-100%','-100%','-100%'];
 
-function left() {
-    tabLocate.unshift(tabLocate.pop());
-    for (let i = 0; i < tabLocate.length; i++) {
-        tabs[i].style.left=tabLocate[i];
-    }
-    window.scrollTo(0,0);
-}
+
 function right() {
-    tabLocate.push(tabLocate.shift());
+    // tabs[0].style.zIndex--;
+    tabLocate.unshift(tabLocate.pop());
+    // tabs[0].style.zIndex--;
     for (let i = 0; i < tabLocate.length; i++) {
         tabs[i].style.left=tabLocate[i];
     }
     window.scrollTo(0,0);
+    updateFooter()
 }
+function left() {
+    // tabs[0].style.zIndex--;
+    tabLocate.push(tabLocate.shift());
+    // tabs[0].style.zIndex--;
+    for (let i = 0; i < tabLocate.length; i++) {
+        tabs[i].style.left=tabLocate[i];
+    }
+    window.scrollTo(0,0);
+    updateFooter();
+}
+function updateFooter() {
+    let loc=tabLocate.indexOf('0');
+    radios[loc].checked=true;
+}
+
+let radioCase=document.getElementById('radioCase');
+function tabSelect(radioValue) {
+    for (let i = 0; i < radioCase.children.length; i++) {
+        if (tabLocate[radioValue]==='0'){
+            break;
+        } else {
+            right();
+        }
+    }
+}
+
+let menuOpen=false;
+
+
+
